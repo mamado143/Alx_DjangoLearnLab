25c9aa6a459b3906037ee260fce47e559c3c2f6a
Mohamed Dahir Mohamoud <midodahir123@gmail.com>
2026-01-26 01:18:27 +0300

    add  advanced_features_and_security	tasks update 12

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
-from .forms import ExampleForm
+
 from django.utils.translation import gettext_lazy as _
 
 class CustomUserManager(BaseUserManager):
