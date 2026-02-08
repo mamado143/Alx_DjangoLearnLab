bb5535d996b79dff8e9a275948e6f8db0430a0f5
Mohamed Dahir Mohamoud <midodahir123@gmail.com>
2026-02-08 15:44:07 +0300

    modified:   LibraryProject/__pycache__/settings.cpython-39.pyc 	modified:   relationship_app/__pycache__/__init__.cpython-39.pyc 	modified:   relationship_app/__pycache__/admin.cpython-39.pyc 	modified:   relationship_app/__pycache__/apps.cpython-39.pyc 	modified:   relationship_app/__pycache__/models.cpython-39.pyc 	modified:   relationship_app/migrations/0001_initial.py 	modified:   relationship_app/migrations/__pycache__/0001_initial.cpython-39.pyc 	modified:   relationship_app/migrations/__pycache__/__init__.cpython-39.pyc 	modified:   relationship_app/models.py 	modified:   relationship_app/query_samples.py

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
+# Author Model
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
+# Book Model (ForeignKey to Author)
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
@@ -13,6 +15,7 @@ class Book(models.Model):
     def __str__(self):
         return self.title
 
+# Library Model (ManyToMany to Book)
 class Library(models.Model):
     name = models.CharField(max_length=100)
     books = models.ManyToManyField(Book, related_name='libraries')
@@ -20,6 +23,7 @@ class Library(models.Model):
     def __str__(self):
         return self.name
 
+# Librarian Model (OneToOne to Library)
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name='librarian')
