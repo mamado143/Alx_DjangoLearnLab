7b2d06c1a2922ff9dc98601bb160095a86879da3
Mohamed Dahir Mohamoud <midodahir123@gmail.com>
2026-02-08 12:55:57 +0300

    modified:   advanced_api_project/__pycache__/urls.cpython-39.pyc 	modified:   api/urls.py

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('books/', BookListView.as_view(), name='book-list'),
     path('books/create/', BookCreateView.as_view(), name='book-create'),
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
-    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
-    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
+    # Change these two lines:
+    path('books/update/', BookUpdateView.as_view(), name='book-update'),
+    path('books/delete/', BookDeleteView.as_view(), name='book-delete'),
 ]
