1201400c052f4befb965f861cdab7640535a9dbe
Mohamed Dahir Mohamoud <midodahir123@gmail.com>
2026-02-08 13:00:16 +0300

    Update and   modified: 	modified:   api/views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,4 +1,8 @@
 from rest_framework import generics
+# Add these imports
+from rest_framework import filters
+from django_filters import rest_framework as django_filters
+
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
@@ -7,6 +11,15 @@ class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+    
+    # 1. Add Filter Backends here
+    filter_backends = [
+        django_filters.DjangoFilterBackend, 
+        filters.SearchFilter, 
+        filters.OrderingFilter
+    ]
+    
+    # 2. Your existing field definitions will now work!
     filterset_fields = ['title', 'author__name', 'publication_year']
     search_fields = ['title', 'author__name']
     ordering_fields = ['title', 'publication_year']
