ef48a37acda3fe0cda04e5cf7aee4e322b1885e1
Mohamed Dahir Mohamoud <midodahir123@gmail.com>
2026-02-08 16:30:45 +0300

    new file:   LibraryProject/relationship_app/forms.py 	modified:   LibraryProject/relationship_app/models.py 	new file:   LibraryProject/relationship_app/templates/relationship_app/add_book.html 	new file:   LibraryProject/relationship_app/templates/relationship_app/delete_book.html 	new file:   LibraryProject/relationship_app/templates/relationship_app/edit_book.html 	modified:   LibraryProject/relationship_app/urls.py 	modified:   LibraryProject/relationship_app/views.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -2,6 +2,22 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey('Author', on_delete=models.CASCADE, related_name='books')
+
+    class Meta:
+        permissions = (
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
+        )
+
+    def __str__(self):
+        return self.title
+
 # Author Model
 class Author(models.Model):
     name = models.CharField(max_length=100)
