1418af9f374c1432c51d1be00d07b90a9dada084
Mohamed Dahir Mohamoud <midodahir123@gmail.com>
2026-02-21 17:30:04 +0300

    modified:   django_blog/__pycache__/settings.cpython-39.pyc 	modified:   django_blog/__pycache__/urls.cpython-39.pyc 	modified:   django_blog/settings.py 	modified:   templates/registration/logout.html

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -77,19 +77,12 @@ WSGI_APPLICATION = "django_blog.wsgi.application"
 # ========================
 # DATABASE CONFIGURATION
 # ========================
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'django_blog_db',
-        'USER': 'postgres',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
 # ========================
 # PASSWORD VALIDATION
 # ========================
