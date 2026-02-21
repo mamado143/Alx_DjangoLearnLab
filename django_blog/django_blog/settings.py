99faf25d3c4f2edcd4b05d20a4916e4fdfdf0184
Mohamed Dahir Mohamoud <midodahir123@gmail.com>
2026-02-21 16:38:47 +0300

    modified:   django_blog/settings.py

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -41,8 +41,12 @@ INSTALLED_APPS = [
 ]
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_blog_db',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
