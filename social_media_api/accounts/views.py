c3394d6704e90e7d56f65bae892c686b4cabbe91
Mohamed Dahir Mohamoud <midodahir123@gmail.com>
2026-02-21 15:24:42 +0300

    modified:   accounts/__pycache__/views.cpython-39.pyc 	modified:   accounts/views.py

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,15 +1,13 @@
 from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
-from django.contrib.auth import authenticate
-from .serializers import RegisterSerializer, LoginSerializer, UserSerializer
-from .models import CustomUser
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated   # ← ADD THIS LINE
+from django.contrib.auth import authenticate
 from django.shortcuts import get_object_or_404
 from django.contrib.auth import get_user_model
-from rest_framework.permissions import IsAuthenticated
+from .serializers import RegisterSerializer, LoginSerializer, UserSerializer
+from .models import CustomUser
 
 User = get_user_model()
 
